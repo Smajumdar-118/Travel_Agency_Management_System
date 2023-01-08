5cc6f5c17ca5f7ddabf934f9b8f9dc5f86aaa9a3
Sayan <sayanmajumdar4444@gmail.com>
2023-01-08 12:16:48 +0530

    Log in Page Changed

--- TravelWithUs2.php
+++ TravelWithUs2.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MoveWithUs.com</title>
-    <link rel="stylesheet" href="Transport.css">
+    <link rel="stylesheet" href="TravelWithUs.css">
 
 </head>
 
--- signup.php
+++ signup.php
@@ -92,6 +92,69 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     z-index: -1;
     opacity: 0.4;
   }
+
+
+
+  
+
+
+  body{
+        
+        background: url(https://images.pexels.com/photos/230794/pexels-photo-230794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1) no-repeat center center fixed;
+        background-size: cover; 
+    }
+    .container{ 
+        color: white;
+        position: absolute;
+        top: 25%;
+        left: 40%; 
+    }
+    .container h1{
+        font-family: 'Heebo', sans-serif;
+        width: 50%;
+        display: inline-block;
+        font-size: 40px;
+        border-bottom: 5px solid purple;
+        margin-bottom: 15px;
+        padding: 13px 0;
+    }
+    .box{
+        width: 50%; 
+        margin: 22px 0px;
+        border-bottom: 2px solid purple;
+    }
+    .box input{  
+        background: none;
+        width: 75%;
+        padding: 5px 10px;
+        font-size: 20px;
+        border: none;
+        outline:none;
+        color: white;
+    }
+    .btn{
+        color: white;
+        cursor: pointer;
+        outline: none; 
+        margin: 12px 0;
+        padding: 10px 21px;
+        border: 2px solid purple;
+        border-radius: 10px;
+        font-size: 18px;
+        background: none;
+        font-weight: bold;
+        
+    }
+    .box i{ 
+        width: 25px; 
+        text-align: center;
+    }
+    .btn:hover{
+        opacity: 0.5;
+        color: purple; 
+        background: white; 
+        
+    }
 </style>
 
 <body>
@@ -122,26 +185,24 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
   if($showError){
   
     echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    <strong>Success!</strong>'.$showError.'
+    <strong>Sorry!</strong>'.$showError.'
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>';
     }
 ?>
 
-  <div class="container my-4">
-    <h1 class="text-center">Sign Up to Our website</h1>
     <form action="/College Project/signup.php" method="POST">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your UserName with anyone else.</small>
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" name="password">
-      </div>
-
-      <button type="submit" class="btn btn-primary">sign Up</button>
+    <div class="container">
+        <h1>Sign Up To MoveWithUS.com</h1>
+        <div class="box">
+            <i class="fa fa-envelope"></i>
+            <input type="text" name="username" id="username" placeholder="Enter Your Username">
+        </div>
+        <div class="box">
+            <i class="fa fa-key"></i>
+            <input type="password" name="password" id="password" placeholder="Enter Your Password">
+        </div>
+        <button class="btn">Sign Up</button>
       </form>
   </div>
 </body>
